1afe9b1574a4b43e77f4ccd6507f336274a19931
yaggidev <lukasz.wojciech.jagiello@gmail.com>
2020-02-06 23:41:56 +0100

    DzieloFormType modifications

--- src/Controller/DzieloController.php
+++ src/Controller/DzieloController.php
@@ -91,7 +91,7 @@ class DzieloController extends AbstractController
         $autor_dziela = $this->getDoctrine()->getRepository(Autor_dzielo::class)->findOneBy(['dzielo_id'=>10]);
         var_dump($dzielo->getId());
         $id = $autor_dziela->getId();
-        $autor = $this->getDoctrine()->getRepository(Autor::class)->findAll();
+        $autor = $this->getDoctrine()->getRepository(Autor::class)->findOneBy(['id'=>$id]);
         return $this->render('dzielo/edit.html.twig', [
             'dzielo' => $dzielo,
             'autorzy' => $autor,
