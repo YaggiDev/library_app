abf37c3973e0d34fd90e8130b7e51bff9e734e41
yaggidev <lukasz.wojciech.jagiello@gmail.com>
2020-02-15 23:11:27 +0100

    PrywatneDzielo.php modifications

--- src/Entity/PrywatneDzielo.php
+++ src/Entity/PrywatneDzielo.php
@@ -16,8 +16,53 @@ class PrywatneDzielo
      */
     private $id;
 
+    /**
+     * @ManyToOne(targetEntity="Dzielo")
+     * @ORM\JoinColumn(name="dzielo_id", referencedColumnName="id")
+     */
+    private $dzielo_id;
+
+    /**
+     * @ManyToOne(targetEntity="Uzytkownik")
+     * @ORM\JoinColumn(name="uzytkownik_id", referencedColumnName="id")
+     */
+    private $uzytkownik_id;
+
     public function getId(): ?int
     {
         return $this->id;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getDzieloId()
+    {
+        return $this->dzielo_id;
+    }
+
+    /**
+     * @param mixed $dzielo_id
+     */
+    public function setDzieloId($dzielo_id): void
+    {
+        $this->dzielo_id = $dzielo_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUzytkownikId()
+    {
+        return $this->uzytkownik_id;
+    }
+
+    /**
+     * @param mixed $uzytkownik_id
+     */
+    public function setUzytkownikId($uzytkownik_id): void
+    {
+        $this->uzytkownik_id = $uzytkownik_id;
+    }
+
 }
